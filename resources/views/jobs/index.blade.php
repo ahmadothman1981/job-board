f03b132d161e723d3882157738948a4f0e7a3ad0
ahmadothman1981 <ahmadothman@outlook.com>
2024-08-11 22:35:34 +0300

    filter experience using radio buttons

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -8,16 +8,38 @@
     <div class="mb-4 grid grid-cols-2 gap-4">
         <div>
             <div class="mb-1 font-semibold">Search</div>
-            <x-text-input type="text" placeholder="Search for a text" name="search" value=""/>
+            <x-text-input type="text" placeholder="Search for a text" name="search" value="{{request('search')}}"/>
         </div>
         <div>
         <div class="mb-1 font-semibold">Salary</div>
         <div class="flex space-x-2">
-        <x-text-input type="text" placeholder="From" name="min_salary" value=""/>
-        <x-text-input type="text" placeholder="To" name="max_salary" value=""/>
+        <x-text-input type="text" placeholder="From" name="min_salary" value="{{request('min_salary')}}"/>
+        <x-text-input type="text" placeholder="To" name="max_salary" value="{{request('max_salary')}}"/>
         </div>
         </div>
+    <div>
+    <div class="mb-1 font-semibold">Experience</div>
+    <label for="experience" class=" mb-1 flex items-center">
+        <input type="radio" name="experience" value=""  @checked(!request('experience'))/>
+        <span class="ml-2">All</span>
+    </label>
 
+    <label for="beginner" class=" mb-1 flex items-center">
+        <input type="radio" name="experience" value="beginner"  @checked(request('experience') === 'beginner')/>
+        <span class="ml-2">Beginner</span>
+    </label>
+
+    <label for="intermediate" class=" mb-1 flex items-center">
+        <input type="radio" name="experience" value="intermediate" @checked(request('experience') === 'intermediate')/>
+        <span class="ml-2">Intermediate</span>
+    </label>
+
+    <label for="expert" class=" mb-1 flex items-center">
+        <input type="radio" name="experience" value="expert" @checked(request('experience') === 'expert') />
+        <span class="ml-2">Expert</span>
+    </label>
+    </div>
+    <div>4</div>
     </div>  
     <button class="w-full">Filter</button>
     </form>  
