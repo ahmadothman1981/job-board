2aba9bfa3817d2165f127133617ca227d651a236
ahmadothman1981 <ahmadothman@outlook.com>
2024-08-12 11:30:53 +0300

    filter category and clear search vlaues using javascript

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -2,44 +2,31 @@
 <x-breadcrumbs  class="mb-4"
 :links="['Jobs'=> route('jobs.index')]"/>
 <x-card class="mb-4 text-sm">
-    <form action="{{route('jobs.index')}}" method="get">
+    <form id="filtering-form" action="{{route('jobs.index')}}" method="get">
 
     
     <div class="mb-4 grid grid-cols-2 gap-4">
         <div>
             <div class="mb-1 font-semibold">Search</div>
-            <x-text-input type="text" placeholder="Search for a text" name="search" value="{{request('search')}}"/>
+            <x-text-input type="text" placeholder="Search for a text" name="search"
+             value="{{request('search')}}"  form-id="filtering-form"/>
         </div>
         <div>
         <div class="mb-1 font-semibold">Salary</div>
         <div class="flex space-x-2">
-        <x-text-input type="text" placeholder="From" name="min_salary" value="{{request('min_salary')}}"/>
-        <x-text-input type="text" placeholder="To" name="max_salary" value="{{request('max_salary')}}"/>
+        <x-text-input type="text" placeholder="From" name="min_salary" value="{{request('min_salary')}}" form-id="filtering-form"/>
+        <x-text-input type="text" placeholder="To" name="max_salary" value="{{request('max_salary')}}" form-id="filtering-form"/>
         </div>
         </div>
     <div>
     <div class="mb-1 font-semibold">Experience</div>
-    <label for="experience" class=" mb-1 flex items-center">
-        <input type="radio" name="experience" value=""  @checked(!request('experience'))/>
-        <span class="ml-2">All</span>
-    </label>
-
-    <label for="beginner" class=" mb-1 flex items-center">
-        <input type="radio" name="experience" value="beginner"  @checked(request('experience') === 'beginner')/>
-        <span class="ml-2">Beginner</span>
-    </label>
-
-    <label for="intermediate" class=" mb-1 flex items-center">
-        <input type="radio" name="experience" value="intermediate" @checked(request('experience') === 'intermediate')/>
-        <span class="ml-2">Intermediate</span>
-    </label>
-
-    <label for="expert" class=" mb-1 flex items-center">
-        <input type="radio" name="experience" value="expert" @checked(request('experience') === 'expert') />
-        <span class="ml-2">Expert</span>
-    </label>
+    <x-radio-group name="experience"  :options="array_combine(array_map('ucfirst', \App\Models\Job::$experience),\App\Models\Job::$experience)"/>
+   
     </div>
-    <div>4</div>
+    <div>
+    <div class="mb-1 font-semibold">Category</div>
+    <x-radio-group name="category"  :options="\App\Models\Job::$categories"/>
+    </div>
     </div>  
     <button class="w-full">Filter</button>
     </form>  
