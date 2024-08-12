80a0b580454deef847839651a21292658ef471a5
ahmadothman1981 <ahmadothman@outlook.com>
2024-08-12 12:45:53 +0300

    download alpine js and refactor the clearing filter

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,21 +1,21 @@
 <x-layout >
 <x-breadcrumbs  class="mb-4"
 :links="['Jobs'=> route('jobs.index')]"/>
-<x-card class="mb-4 text-sm">
-    <form id="filtering-form" action="{{route('jobs.index')}}" method="get">
+<x-card class="mb-4 text-sm" x-data="">
+    <form x-ref="filters" id="filtering-form" action="{{route('jobs.index')}}" method="get">
 
     
     <div class="mb-4 grid grid-cols-2 gap-4">
         <div>
             <div class="mb-1 font-semibold">Search</div>
             <x-text-input type="text" placeholder="Search for a text" name="search"
-             value="{{request('search')}}"  form-id="filtering-form"/>
+             value="{{request('search')}}"  form-ref="filters"/>
         </div>
         <div>
         <div class="mb-1 font-semibold">Salary</div>
         <div class="flex space-x-2">
-        <x-text-input type="text" placeholder="From" name="min_salary" value="{{request('min_salary')}}" form-id="filtering-form"/>
-        <x-text-input type="text" placeholder="To" name="max_salary" value="{{request('max_salary')}}" form-id="filtering-form"/>
+        <x-text-input type="text" placeholder="From" name="min_salary" value="{{request('min_salary')}}" form-ref="filters"/>
+        <x-text-input type="text" placeholder="To" name="max_salary" value="{{request('max_salary')}}" form-ref="filters"/>
         </div>
         </div>
     <div>
@@ -28,7 +28,7 @@
     <x-radio-group name="category"  :options="\App\Models\Job::$categories"/>
     </div>
     </div>  
-    <button class="w-full">Filter</button>
+    <x-button class="w-full"> Filter </x-button>
     </form>  
 </x-card>
     @foreach($jobs as $job)
