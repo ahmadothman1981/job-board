4059a4ceb6071dfbad2a5a4cf0bf45248cf793bc
ahmadothman1981 <ahmadothman@outlook.com>
2024-08-14 19:11:07 +0300

    create employer controller and creting employer policy

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EmployerController;
 use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\MyJobApplicationController;
 
@@ -26,4 +27,5 @@ Route::delete('auth', [AuthController::class, 'destroy'])->name('auth.destroy');
 Route::middleware('auth')->group(function(){
     Route::resource('jobs.applications',JobApplicationController::class)->only(['create','store']);
     Route::resource('my-job-applications',MyJobApplicationController::class)->only(['index','destroy']);
+    Route::resource('employer',EmployerController::class)->only(['create','store']);
 });
